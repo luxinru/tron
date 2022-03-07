6b8526e718018f956447ab14e058c5af306bcb86
luxinru <865666015@qq.com>
2022-03-07 17:44:38 +0800

    tron: 页面名称不需要动态展示

--- src/main.js
+++ src/main.js
@@ -103,14 +103,6 @@ Vue.use(Vant);
 // }
 Vue.config.productionTip = false
 
-router.beforeEach((to, from, next) => {
-  /* 路由发生变化修改页面title */
-  if (to.meta.title) {
-    document.title = to.meta.title
-  }
-  next()
-});
-
 new Vue({
   i18n,
   router,
