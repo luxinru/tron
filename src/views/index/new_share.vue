84186e0e4767c91655d5f0d4e062d6cea66142c4
Xinru Lu <865666015@qq.com>
2022-03-08 19:48:19 +0800

    完成share

--- src/views/index/new_share.vue
+++ src/views/index/new_share.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="page_root">
-    <section class="header">{{ $t('share') }}</section>
-
     <section class="content">
-      <img
-        class="logo"
-        src="@/assets/tron/Share_slices/3a55a99b40b8d458de6b526f5639df0ca17c7c4eb6e22-ApSyI5_fw1200.png"
-        alt=""
-      />
-
       <span class="title">{{ $t('invite_your_contacts') }}</span>
       <span class="sub_title">{{ $t('or_friends_and_earn_rewards') }}</span>
+
+      <div class="info">
+        <span>{{ $t('refer_and_earn_rewards') }}</span>
+        <span>{{ $t('share_your_referal_link_and_start_earning') }}</span>
+      </div>
 
       <div class="code_box">
         <div class="code">
@@ -22,17 +19,6 @@
 
       <div class="btn" :data-clipboard-text="data.register_url" @click="copy">
         {{ $t('copy_link') }}
-      </div>
-
-      <div class="refer">
-        <img
-          src="@/assets/tron/Share_slices/b65ec548fe3e657e98c9ab73ae09d784724255f312716-LwzSb4.png"
-          alt=""
-        />
-        <div class="info">
-          <span>{{ $t('refer_and_earn_rewards') }}</span>
-          <span>{{ $t('share_your_referal_link_and_start_earning') }}</span>
-        </div>
       </div>
     </section>
   </div>
@@ -60,13 +46,13 @@ export default {
     copy() {
       var clipboard = new Clipboard('.btn')
       clipboard.on('success', (e) => {
-        this.$notify({type: 'success', message: this.$t('copy_successfully')})
+        this.$notify({ type: 'success', message: this.$t('copy_successfully') })
         //  释放内存
         clipboard.destroy()
       })
       clipboard.on('error', (e) => {
         // 不支持复制
-        this.$notify({type: 'danger', message: this.$t('copy_failed')})
+        this.$notify({ type: 'danger', message: this.$t('copy_failed') })
         // 释放内存
         clipboard.destroy()
       })
@@ -82,138 +68,96 @@ export default {
 
 <style lang="less" scoped>
 .page_root {
-  background: linear-gradient(40deg, #5e63e7 0%, #be92fe 100%);
   width: 100%;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  padding: 0 13px 16px;
-
-  .header {
-    width: 100%;
-    height: 50px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 17px;
-    font-family: PingFang SC;
-    font-weight: 500;
-    color: rgba(255, 255, 255, 1);
-    background-color: transparent;
-  }
+  background: url('~@/assets/tron/组 31.png') no-repeat;
+  background-size: 100% 812px;
+  margin-bottom: 80px;
 
   .content {
     width: 100%;
     padding: 10px 18px;
-    background: #ffffff;
-    box-shadow: 0px 6px 10px 0px rgba(19, 19, 20, 0.06);
     border-radius: 13px;
     display: flex;
     flex-direction: column;
-    align-items: center;
-
-    .logo {
-      width: 300px;
-      height: 138px;
-      margin: 46px 0 0 65px;
-    }
 
     .title {
-      font-size: 21px;
+      width: 100%;
+      font-size: 33px;
       font-family: Arial;
       font-weight: bold;
-      color: #394249;
-      margin-top: 24px;
+      color: #ffffff;
+      line-height: 39px;
+      margin-top: 96px;
     }
 
     .sub_title {
-      font-size: 13px;
-      font-family: PingFang SC;
+      width: 100%;
+      font-size: 17px;
+      font-family: Arial;
       font-weight: 400;
-      color: #4a5156;
-      margin-top: 11px;
+      color: rgba(255, 255, 255, 0.7);
+      line-height: 20px;
+      margin-top: 15px;
     }
 
     .code_box {
-      width: 131px;
-      height: 131px;
-      background: rgba(180, 140, 255, 0.5);
-      border-radius: 7px;
-      margin-top: 25px;
+      width: 130px;
+      height: 130px;
+      background: #fff;
+      margin-top: 59px;
       display: flex;
       align-items: center;
       justify-content: center;
 
       .code {
-        width: 112px;
-        height: 112px;
+        width: 128px;
+        height: 128px;
         background-color: #fff;
         .qrcode {
-          width: 112px;
-          height: 112px;
+          width: 128px;
+          height: 128px;
         }
       }
     }
 
     .url {
-      margin-top: 18px;
+      width: 158px;
       font-size: 11px;
       font-family: PingFang SC;
       font-weight: 400;
-      color: rgba(0, 0, 0, 0.5);
+      color: rgba(255, 255, 255, 0.6);
+      line-height: 16px;
+      margin-top: 24px;
     }
 
     .btn {
-      padding: 0 16px;
-      height: 35px;
-      background: #5e63e7;
-      border-radius: 17px;
-      font-size: 17px;
+      width: 124px;
+      height: 40px;
+      background: #FFFFFF;
+      border-radius: 3px;
+      font-size: 16px;
       font-family: PingFang SC;
-      font-weight: 600;
-      color: #ffffff;
+      font-weight: 500;
+      color: #D50A0A;
       display: flex;
       align-items: center;
       justify-content: center;
-      margin-top: 20px;
+      margin-top: 21px;
     }
 
-    .refer {
-      width: 312px;
-      background: rgba(97, 100, 232, 0.2);
-      border-radius: 20px;
+    .info {
+      width: 100%;
       display: flex;
-      align-items: center;
-      margin-top: 20px;
-      padding: 8px 20px;
-
-      img {
-        width: 49px;
-        height: 58px;
-        margin-right: 16px;
-      }
-
-      .info {
-        flex: 1 0;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        font-size: 17px;
-        font-family: Arial;
-        font-weight: bold;
-        color: #1e253c;
-
-        span {
-          &:last-child {
-            margin-top: 10px;
-            font-size: 11px;
-            font-family: Arial;
-            font-weight: 400;
-            color: #5e63e7;
-          }
-        }
-      }
+      flex-direction: column;
+      font-size: 19px;
+      font-family: Source Han Sans CN;
+      font-weight: bold;
+      color: rgba(255, 255, 255, 0.8);
+      line-height: 32px;
+      margin-top: 77px;
     }
   }
 }
